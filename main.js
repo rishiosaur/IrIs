9e99f1558f3eb4c0912d3aa7aeb12ff16e19daff
Rishi Kothari <rishi2019@wgdavis.ca>
2017-03-01 07:23:46 -0500

    Moved Contributing.md file, started work on the random function, added .toUpperCase

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function IrIs(input, output, button) {
   'use strict'
   //Variables
 
-  /*The JSON array that is returned */var parseJSON = ["init":true,];
+  var parseJSON = ["init":true];
   var outflow = document.getElementById(output);
   function crElemWithClass(val, str) {
     var elem = createElement("span");
@@ -19,15 +19,30 @@ function IrIs(input, output, button) {
   //Adds something to the JSON array, which is stringified at the end.
   function ifParseJSON(toBeAdded, arrayCheck) {
     if (arrayCheck.every() != toBeAdded) {
-      arrayCheck.push(toBeAdded)
+      arrayCheck.push(toBeAdded);
+    }
+  }
+  //The random function
+  function randString(case1, case2, case3) {
+    var rand = Math.random(0,3);
+    rand = Math.floor(rand);
+    switch (rand) {
+      case 2:
+        return case2
+        break;
+      default:
+        return case1
+
     }
   }
   //The while loop keeps on running the switch block when the button is running
   while (button.onMouseDown()) {
       convElem(input.val, "self");
+      var inputUpper = input.val;
+      inputUpper.toUpperCase();
       //The switch block that evaluates the possibilities
       switch (input.val()) {
-        case "hi":
+        case "HI" || "HELLO" || "GREETINGS":
           convElem("Greetings", "IrIs");
           ifParseJSON("Greeted":true, parseJSON);
         break;
